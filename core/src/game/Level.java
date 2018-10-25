e75c50f1583df9fccb948209eaad01bf50c42827
Ernestas Mitkus <ernestas@zenitech.co.uk>
2018-10-25 11:44:04 +0300

    Add box2d binding to Ball class and add wall collision

--- core/src/game/Level.java
+++ core/src/game/Level.java
@@ -10,9 +10,8 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.MassData;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import game.entities.Ball;
 import game.entities.Wall;
@@ -27,6 +26,9 @@ public class Level extends ScreenAdapter {
     private static final int VELOCITY_ITERATIONS = 106;
     private static final int POSITION_ITERATIONS = 102;
 
+    private final float wallSize;
+    private final float ballSize;
+
     private World world;
     private Box2DDebugRenderer b2dr;
 
@@ -35,19 +37,68 @@ public class Level extends ScreenAdapter {
 
     private List<Wall> walls = new ArrayList<>();
     private Ball ball;
-
-    private Body boxx;
+    private Body ballBody;
 
     public Level(TextureAtlas atlas) {
-        generateWalls(atlas, Gdx.graphics.getWidth() / 50f);
-        ball = new Ball(Gdx.graphics.getWidth() >> 1, Gdx.graphics.getHeight() >> 1, atlas);
+        wallSize = Gdx.graphics.getWidth() / 50f;
+        ballSize = 16f;
 
-        world = new World(new Vector2(0, -9.81f), true);
+        generateWalls(atlas, wallSize);
+        ball = new Ball(Gdx.graphics.getWidth() >> 1, Gdx.graphics.getHeight() >> 1, ballSize, atlas);
+
+        world = new World(new Vector2(0, 0), true);
         b2dr = new Box2DDebugRenderer();
 
         batch = new SpriteBatch();
         cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.setToOrtho(false, Gdx.graphics.getWidth() / PPM, Gdx.graphics.getHeight() / PPM);
+
+        BodyDef bdef = new BodyDef();
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(ball.getPosition().x / PPM, ball.getPosition().y / PPM);
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(ballSize / PPM);
+
+        FixtureDef fdef = new FixtureDef();
+        fdef.shape = shape;
+        fdef.restitution = 1f;
+        fdef.density = 0.8f;
+        fdef.friction = 0;
+
+        ballBody = world.createBody(bdef);
+        ballBody.createFixture(fdef);
+        ballBody.setBullet(true);
+
+        ball.setBox2DBody(ballBody);
+        ballBody.setUserData(ball);
+
+        // walls
+        bdef.type = BodyDef.BodyType.StaticBody;
+        bdef.position.set(0, 0);
+
+        EdgeShape edgeShape = new EdgeShape();
+        fdef.shape = edgeShape;
+        Body walls = world.createBody(bdef);
+
+
+        // bottom
+        edgeShape.set(wallSize / PPM, wallSize / PPM, (Gdx.graphics.getWidth() - wallSize) / PPM, wallSize / PPM);
+        walls.createFixture(fdef);
+
+        // left
+        edgeShape.set(wallSize / PPM, wallSize / PPM, wallSize / PPM, (Gdx.graphics.getHeight() - wallSize) / PPM);
+        walls.createFixture(fdef);
+
+        // right
+        edgeShape.set((Gdx.graphics.getWidth() - wallSize) / PPM, wallSize / PPM, (Gdx.graphics.getWidth() - wallSize) / PPM, (Gdx.graphics.getHeight() - wallSize) / PPM);
+        walls.createFixture(fdef);
+
+        // left
+        edgeShape.set(wallSize / PPM, (Gdx.graphics.getHeight() - wallSize) / PPM, (Gdx.graphics.getWidth() - wallSize) / PPM, (Gdx.graphics.getHeight() - wallSize) / PPM);
+        walls.createFixture(fdef);
+
+        ballBody.applyForce(500f, 300f, ballBody.getWorldCenter().x, ballBody.getWorldCenter().y, true);
     }
 
     @Override
@@ -58,6 +109,7 @@ public class Level extends ScreenAdapter {
 
     private void update(float delta) {
         world.step(delta, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        ball.update(delta);
     }
 
     private void render(SpriteBatch batch) {
--- core/src/game/entities/Ball.java
+++ core/src/game/entities/Ball.java
@@ -3,6 +3,11 @@ package game.entities;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+
+import static game.B2DVars.PPM;
 
 public class Ball {
 
@@ -10,15 +15,66 @@ public class Ball {
 
     private Sprite sprite;
 
-    public Ball(float x, float y, TextureAtlas atlas) {
+    private Body box2DBody;
+
+    public Ball(float x, float y, float radius, TextureAtlas atlas) {
         sprite = atlas.createSprite(BALL_SPRITE_NAME);
-        sprite.setPosition(x, y);
+        sprite.setOriginBasedPosition(x, y);
+        setSpriteRadius(radius);
     }
 
-    public void push(float vx, float vy) {
+    public void update(float delta) {
+        if (box2DBody != null) {
+            // position
+            Vector2 worldCenter = box2DBody.getWorldCenter();
+            setSpritePosition(new Vector2(worldCenter.x * PPM, worldCenter.y * PPM));
+
+            // size
+            setSpriteRadius(getCircleShape().getRadius() * PPM);
+
+            // angle
+            setSpriteRotation(box2DBody.getAngle());
+        }
     }
 
     public void render(SpriteBatch batch) {
         sprite.draw(batch);
+    }
+
+    public Vector2 getPosition() {
+        return new Vector2(
+            sprite.getX() + sprite.getOriginX(),
+            sprite.getY() + sprite.getOriginY());
+    }
+
+    private void setSpritePosition(Vector2 pos) {
+        sprite.setOriginBasedPosition(pos.x, pos.y);
+    }
+
+    public float getRadius() {
+        if (box2DBody != null) {
+            return getCircleShape().getRadius() * PPM;
+        }
+        return 0;
+    }
+
+    private void setSpriteRadius(float radius) {
+        sprite.setScale(radius / (sprite.getWidth() / 2), radius / (sprite.getHeight() / 2));
+    }
+
+    private void setSpriteRotation(float angle) {
+        sprite.setRotation(angle);
+    }
+
+    public void setBox2DBody(Body ballBody) {
+        this.box2DBody = ballBody;
+        update(0f);
+    }
+
+    private CircleShape getCircleShape() {
+        if (box2DBody != null) {
+            return (CircleShape) box2DBody.getFixtureList().get(0).getShape();
+        }
+        return null;
     }
 }
